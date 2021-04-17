c9924e41816367151c1b8087a46aaeaa2846c971
André Alvim <andre@alvim.net>
2021-04-16 22:15:18 -0300

    initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Hi</h1>
+    </>
+  )
+}

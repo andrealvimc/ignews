4cb33552d4cf125550b974188525ab428b007bd3
André Alvim <andre@alvim.net>
2021-04-17 16:35:17 -0300

    ✨ feat: webhooks do stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
+import { signIn, useSession, signOut } from 'next-auth/client'
 import styles from './styles.module.scss';
 
 
 export function SignInButton() {
-  const isUserLogeedIn = true;
+  const [session] = useSession();
 
-  return isUserLogeedIn ? (
-    <button type="button" className={styles.SignInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.SignInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      André Alvim
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.SignInButton}>
+    <button
+      type="button"
+      className={styles.SignInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign In with Github
     </button >
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header'
+import { Provider as NextAuhProvider } from 'next-auth/client';
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuhProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuhProvider>
   )
 }
 
